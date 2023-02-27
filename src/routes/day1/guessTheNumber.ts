f12fe44fd10a84d6fffc534c4599b639cdd67696
Kamal Singh <kamalsingh200238@gmail.com>
2023-02-28 00:04:16 +0530

    code for game logic (still pending)

--- src/routes/day1/guessTheNumber.ts
+++ src/routes/day1/guessTheNumber.ts
@@ -1,7 +1,49 @@
 import "../../style.css"
 
-const answer = Math.floor(Math.random() * 10)
+let answer = Math.floor(Math.random() * 10) + 1;
+let chances = 0;
+const numberInput = document.getElementById("number-input") as HTMLInputElement;
+const resetButton = document.getElementById("reset-button") as HTMLButtonElement
+let message = document.getElementById("message")
 
-const userInput = document.getElementById("user-input");
+console.log({ chances, answer })
 
-console.log(userInput?.nodeValue)
+function resetTheGame() {
+  chances = 0;
+  answer = Math.floor(Math.random() * 10) + 1;
+  console.log({ chances, answer })
+}
+
+function displayMessage(m: string) {
+  // let x = message?.innerHTML
+  // x = `${m}`
+}
+
+document.getElementById("number-form")?.addEventListener("submit", (event) => {
+  // prevent form submit
+  event.preventDefault();
+  const guess = Number(numberInput.value)
+
+  // check the if guess === answer
+  // check the if guess > answer
+  // check the if guess <
+  if (guess === answer) {
+    displayMessage("Congrats you won")
+    console.log("you won the game")
+    resetTheGame()
+  } else if (guess > answer) {
+    displayMessage("your guess is higher than the answer")
+    console.log("your guess is higher than the answer")
+  } else if (guess < answer) {
+    displayMessage("your guess is lower than the answer")
+    console.log("your guess is lower than the answer")
+  }
+
+  // increase the chances
+  chances++
+  console.log({ chances, guess })
+})
+
+resetButton.addEventListener("click", () => {
+  resetTheGame()
+})
