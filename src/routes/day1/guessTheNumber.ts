7690ed9c0f2e534ab971224b0dec7cacf3b37975
Kamal Singh <kamalsingh200238@gmail.com>
2023-03-02 20:23:47 +0530

    Now message is displayed inside a p tag in html

--- src/routes/day1/guessTheNumber.ts
+++ src/routes/day1/guessTheNumber.ts
@@ -8,15 +8,23 @@ let message = document.getElementById("message")
 
 console.log({ chances, answer })
 
-function resetTheGame() {
+function resetTheGame(m: string = "New game has been started") {
+  // change chances
   chances = 0;
+  // create new answer
   answer = Math.floor(Math.random() * 10) + 1;
+  // change the text inside the text field
+  numberInput.value = ""
+  // change the message
+  displayMessage(m)
   console.log({ chances, answer })
+
 }
 
 function displayMessage(m: string) {
-  // let x = message?.innerHTML
-  // x = `${m}`
+  if(message) {
+    message.innerText = m
+  }
 }
 
 document.getElementById("number-form")?.addEventListener("submit", (event) => {
@@ -24,24 +32,25 @@ document.getElementById("number-form")?.addEventListener("submit", (event) => {
   event.preventDefault();
   const guess = Number(numberInput.value)
 
-  // check the if guess === answer
-  // check the if guess > answer
   // check the if guess <
   if (guess === answer) {
-    displayMessage("Congrats you won")
-    console.log("you won the game")
+    displayMessage("Congrats you won. And a new game has been started")
     resetTheGame()
-  } else if (guess > answer) {
+
+  } else if (guess > answer) {// check the if guess > answer
     displayMessage("your guess is higher than the answer")
-    console.log("your guess is higher than the answer")
-  } else if (guess < answer) {
+
+  } else if (guess < answer) { // check the if guess === answer
     displayMessage("your guess is lower than the answer")
-    console.log("your guess is lower than the answer")
   }
 
   // increase the chances
   chances++
-  console.log({ chances, guess })
+
+  // check if chances has 
+  if(chances >= 5) {
+    resetTheGame("You lost the previous game. But a new game has been started for you. Good luck this time.")
+  }
 })
 
 resetButton.addEventListener("click", () => {
